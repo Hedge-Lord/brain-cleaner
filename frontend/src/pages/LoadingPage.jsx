8422ee75683f4cdb7ee81d8558b3d81a47e64643
michelleduong35 <michelleduong35@gmail.com>
2025-03-03 18:49:30 -0800

    loading styling complete, background color

--- frontend/src/pages/LoadingPage.jsx
+++ frontend/src/pages/LoadingPage.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './LoadingPage.css';
-import "../../assets/digital-numbers-webfont/style.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LoadingPage.css";
+import "../../assets/digital-numbers-webfont/style.css";
 
 const LoadingPage = () => {
   return (
     <div className="home-container">
       <div className="home-content">
-        <h1 style={{ fontFamily: "'Digital Numbers Regular'", fontWeight: "normal", fontSize: "42px" }}>
+        <h1
+          style={{
+            fontFamily: "'Digital Numbers Regular'",
+            fontWeight: "normal",
+            fontSize: "42px",
+          }}
+        >
           Welcome
         </h1>
-        <p>convert your pdfs to brainrot brain.clnr </p>
+        <p class="subheading">
+          convert your pdfs to <span class="crossed-out">brainrot</span>{" "}
+          brain.clnr
+        </p>
         <div className="cta-buttons">
           <Link to="/login" className="get-started-btn">
             start
