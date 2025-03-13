2fcd003463c6cdc292882dfadf0e8aa8f344f702
dwu006 <happy4danielwu@gmail.com>
2025-03-12 22:44:22 -0700

    works3?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,8 @@ export const useAuth = () => {
   return context;
 };
 
-const API_URL = `http://localhost:3000/api/v1`;
+const API_URL = `https://brain-cleaner-production.up.railway.app/api/v1`;
+
 
 // This component wraps our app and persists the current route
 const RouteTracker = ({ children }) => {
